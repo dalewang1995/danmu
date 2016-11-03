baa19ca57e14b7083ddd29c95de9b5dd9e8c1f7b
dale <dalewang1995@gmail.com>
2016-11-03 23:01:53 +0800

    调用野狗后端实时云
    实现弹幕数据云存储

--- danmu.js
+++ danmu.js
@@ -1,3 +1,7 @@
-function{
-  
-}
+var ref = new Wilddog("https://dale.wilddogio.com/"); //要读写数据，必须先创建 Wilddog 引用
+var arr = [];
+$(".s_sub").click(function() {
+  var text = $(".s_txt").val();
+  ref.child('message').push(text);
+  $(".s_txt").val('');
+});
